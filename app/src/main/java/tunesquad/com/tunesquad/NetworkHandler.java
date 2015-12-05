dee3ce8037110ddf14d45cb6da195cf57cd1996b
shaun.d.clark <shaun.d.clark@accenture.com>
2015-12-05 16:27:49 +0000

    added volley framework and myhumm library

---
+++ app/src/main/java/tunesquad/com/tunesquad/NetworkHandler.java
@@ -0,0 +1,18 @@
+package tunesquad.com.tunesquad;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by shaun.d.clark on 05/12/15.
+ */
+public class NetworkHandler {
+
+
+}
--- app/src/main/java/tunesquad/com/tunesquad/SplashScreen.java
+++ app/src/main/java/tunesquad/com/tunesquad/SplashScreen.java
@@ -3,11 +3,19 @@ package tunesquad.com.tunesquad;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import humm.android.api.HummAPI;
+
 public class SplashScreen extends AppCompatActivity {
+
+    // Scope...
+    private static final String ENDPOINT = "https://www.";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        // This is the main method.. start here ^^ leave the above
+
     }
+
 }
