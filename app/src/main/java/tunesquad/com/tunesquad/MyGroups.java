6da8e4065a35c709c92b8e3507331ff5db3b78c9
shaun.d.clark <shaun.d.clark@accenture.com>
2015-12-05 23:51:37 +0000

    implemented a working version of the youtube player with a default video

--- app/src/main/java/tunesquad/com/tunesquad/MyGroups.java
+++ app/src/main/java/tunesquad/com/tunesquad/MyGroups.java
@@ -1,10 +1,20 @@
 package tunesquad.com.tunesquad;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 /**
  * Created by shaun.d.clark on 05/12/15.
@@ -12,8 +22,14 @@ import android.widget.Button;
 
 public class MyGroups extends AppCompatActivity {
 
-    Button createNewSquad;
-    Button findSqaud;
+    private Button createNewSquad;
+    private Button findSqaud;
+    private VolleyManager vm = new VolleyManager();
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
 
     @Override
@@ -21,14 +37,17 @@ public class MyGroups extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_groups);
 
-        createNewSquad = (Button)findViewById(R.id.create_new_sqaud);
-        findSqaud = (Button)findViewById(R.id.find_sqaud);
+
+        //vm.sendDataToEndpoint(MyGroups.this, stringBuilder("tunesqaudfinal-env.elasticbeanstalk.com/public/index.php?apikey=spacejam"));
+
+        createNewSquad = (Button) findViewById(R.id.create_new_sqaud);
+        findSqaud = (Button) findViewById(R.id.find_sqaud);
 
         findSqaud.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent mainIntent = new Intent(MyGroups.this, FindSquad.class);
+                Intent mainIntent = new Intent(MyGroups.this, PlayMusic.class);
                 MyGroups.this.startActivity(mainIntent);
 
             }
@@ -44,5 +63,25 @@ public class MyGroups extends AppCompatActivity {
             }
         });
 
+    }
+
+    public StringRequest stringBuilder(String url) {
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.i("Network Response", response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // Error handling
+                System.out.println("Something went wrong!");
+                error.printStackTrace();
+
+            }
+        });
+        return stringRequest;
     }
 }
--- app/src/main/java/tunesquad/com/tunesquad/PlayMusic.java
+++ app/src/main/java/tunesquad/com/tunesquad/PlayMusic.java
@@ -1,26 +1,147 @@
 package tunesquad.com.tunesquad;
 
-import android.media.AudioManager;
-import android.media.MediaPlayer;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+        import com.google.android.youtube.player.YouTubeBaseActivity;
+        import com.google.android.youtube.player.YouTubeInitializationResult;
+        import com.google.android.youtube.player.YouTubePlayer;
+        import com.google.android.youtube.player.YouTubePlayer.PlaybackEventListener;
+        import com.google.android.youtube.player.YouTubePlayerFragment;
+        import com.google.android.youtube.player.YouTubePlayer.PlayerStateChangeListener;
+        import com.google.android.youtube.player.YouTubePlayer.Provider;
+        import android.os.Bundle;
+        import android.widget.Toast;
 
-/**
- * Created by shaun.d.clark on 05/12/15.
- */
+public class PlayMusic extends YouTubeBaseActivity implements
+        YouTubePlayer.OnInitializedListener{
 
-public class PlayMusic extends AppCompatActivity {
+    private static final String API_KEY = "AIzaSyDe-hPpzjgQRIBcTPxPATNR8D5Pj5Is1rU";
+    public static final String VIDEO_ID = "o7VVHhK9zf0";
 
+    private YouTubePlayer youTubePlayer;
+    private YouTubePlayerFragment youTubePlayerFragment;
 
-    MediaPlayer mediaPlayer;
+    private static final int RQS_ErrorDialog = 1;
+
+    private MyPlayerStateChangeListener myPlayerStateChangeListener;
+    private MyPlaybackEventListener myPlaybackEventListener;
+
+    String log = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.play_music);
 
-        mediaPlayer = new MediaPlayer();
-        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        youTubePlayerFragment = (YouTubePlayerFragment)getFragmentManager()
+                .findFragmentById(R.id.youtubeplayerfragment);
+        youTubePlayerFragment.initialize(API_KEY, this);
+
+
+        myPlayerStateChangeListener = new MyPlayerStateChangeListener();
+        myPlaybackEventListener = new MyPlaybackEventListener();
+
+    }
+
+    @Override
+    public void onInitializationFailure(Provider provider,
+                                        YouTubeInitializationResult result) {
+
+        if (result.isUserRecoverableError()) {
+            result.getErrorDialog(this, RQS_ErrorDialog).show();
+        } else {
+            Toast.makeText(this,
+                    "YouTubePlayer.onInitializationFailure(): " + result.toString(),
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onInitializationSuccess(Provider provider, YouTubePlayer player,
+                                        boolean wasRestored) {
+
+        youTubePlayer = player;
+
+        Toast.makeText(getApplicationContext(),
+                "YouTubePlayer.onInitializationSuccess()",
+                Toast.LENGTH_LONG).show();
+
+        youTubePlayer.setPlayerStateChangeListener(myPlayerStateChangeListener);
+        youTubePlayer.setPlaybackEventListener(myPlaybackEventListener);
+
+        if (!wasRestored) {
+            player.cueVideo(VIDEO_ID);
+        }
+
+    }
+
+    private final class MyPlayerStateChangeListener implements PlayerStateChangeListener {
+
+        private void updateLog(String prompt){
+
+        };
+
+        @Override
+        public void onAdStarted() {
+            updateLog("onAdStarted()");
+        }
+
+        @Override
+        public void onError(
+                com.google.android.youtube.player.YouTubePlayer.ErrorReason arg0) {
+            updateLog("onError(): " + arg0.toString());
+        }
+
+        @Override
+        public void onLoaded(String arg0) {
+            updateLog("onLoaded(): " + arg0);
+        }
+
+        @Override
+        public void onLoading() {
+            updateLog("onLoading()");
+        }
+
+        @Override
+        public void onVideoEnded() {
+            updateLog("onVideoEnded()");
+        }
+
+        @Override
+        public void onVideoStarted() {
+            updateLog("onVideoStarted()");
+        }
+
+    }
+
+    private final class MyPlaybackEventListener implements PlaybackEventListener {
+
+        private void updateLog(String prompt){
+
+        };
+
+        @Override
+        public void onBuffering(boolean arg0) {
+            updateLog("onBuffering(): " + String.valueOf(arg0));
+        }
+
+        @Override
+        public void onPaused() {
+            updateLog("onPaused()");
+        }
+
+        @Override
+        public void onPlaying() {
+            updateLog("onPlaying()");
+        }
+
+        @Override
+        public void onSeekTo(int arg0) {
+            updateLog("onSeekTo(): " + String.valueOf(arg0));
+        }
+
+        @Override
+        public void onStopped() {
+            updateLog("onStopped()");
+        }
 
     }
 
